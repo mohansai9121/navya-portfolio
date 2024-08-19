a6b381337c59d73c6cece162f47c2491ec921597
Mohan Sai <mohansaichinthala9121@gmail.com>
2024-08-19 14:55:09 +0530

    Descripttion services added to services

--- src/Services/Services.js
+++ src/Services/Services.js
@@ -9,17 +9,26 @@ const Services = () => {
         <h2>My Services</h2>
         <p>These are the experiences I have...</p>
       </div>
-      <div className="projects">
+      <div className="projects1">
         {services.map((service, index) => {
           return (
-            <div key={index} className="project">
-              <h3>
-                {service.title}
-                <br />
-                <span style={{ fontSize: "12px" }}>{service.period}</span>
-              </h3>
-              <h4>{service.company}</h4>
-              <p>{service.description}</p>
+            <div key={index}>
+              <div className="flip-box2">
+                <div className="flip-box-inner2">
+                  <div className="front-view">
+                    <h3>
+                      {service.title}
+                      <br />
+                      <span style={{ fontSize: "12px" }}>{service.period}</span>
+                    </h3>
+                    <h4>{service.company}</h4>
+                    <p>{service.description}</p>
+                  </div>
+                  <div className="back-view">
+                    <p style={{ fontWeight: "500" }}>{service.description2}</p>
+                  </div>
+                </div>
+              </div>
             </div>
           );
         })}
--- src/assests/services.js
+++ src/assests/services.js
@@ -6,6 +6,8 @@ const services = [
       "Configured and managed over 20 Oracle-Apps setups on the client side",
     period: "Feb 2020 – July 2022",
     company: "Cognizant Technology Solutions",
+    description2:
+      "Developed front-end verification scripts using Selenium and back-end processes with PL/SQL, cutting manual effort by 50%. Managed Jenkins pipelines and deployments across three environments, ensuring zero downtime. Led data ingestion team in SCRUM sessions, driving user-story creation and estimationfor project releases.",
   },
   {
     sNo: 2,
@@ -14,6 +16,8 @@ const services = [
       "Assisted in resolving technical concerns, improving customer satisfaction",
     period: "May 2019 – Jan 2020",
     company: "Microsoft",
+    description2:
+      "Assisted in resolving technical concerns, improving customer satisfaction. Developed and organized documentation, supporting team operations. Fostered professional relationships with peers and supervisors, aiding in knowledge exchange.",
   },
 ];
 
